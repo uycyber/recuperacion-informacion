9eaadea66f4dcf938681df8bc198a5ac17af0972
landroh <landroh@c0842110-ac01-369e-c716-31a5553ed276>
2012-06-21 21:02:00 +0000

    segundo commit

--- RI/conexionbd/conexion.php
+++
@@ -1,6 +0,0 @@
-<?php
-	//Conectar a la Base de Datos /////////////////////////////
-	$conexion = mysql_connect("localhost", "root", "");
-	mysql_select_db("documentos", $conexion);
-	///////////////////////////////////////////////////////////
-?>
