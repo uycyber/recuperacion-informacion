535b1bba5f1c9cd3b1becdb8baf66b46e6f026af
landroh <landroh@c0842110-ac01-369e-c716-31a5553ed276>
2012-06-21 22:55:14 +0000

    mkdir corregido y comenté lo de las consultas sql...

--- RI/configuracion.php
+++ RI/configuracion.php
@@ -1,8 +1,4 @@
 <?php
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 
 //Separador de directorios, de acuerdo al sistema operativo
 $separador= DIRECTORY_SEPARATOR;
